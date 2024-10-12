e22ccc27b1a0a88943ce49578fba2c2419b05a11
binmaster-access <142739073+binmaster-access@users.noreply.github.com>
2024-10-12 14:12:47 -0400

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -2,7 +2,7 @@ local owner = "binmaster-access"
 local branch = "revision"
 
 local function webImport(file)
-    return loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/%s/Hydronium/%s/%s.lua"):format(owner, branch, file)), file .. '.lua')()
+    return loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/%s/Hydronium/refs/heads/%s/%s.lua"):format(owner, branch, file)), file .. '.lua')()
 end
 
 webImport("init")
